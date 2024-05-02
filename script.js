e3c786ce5ef3c389a7aacfdfd2f7143d9f42ecf4
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-05-02 19:17:28 +0300

    hw-3 part 1/2

--- script.js
+++ script.js
@@ -1,49 +1,9 @@
-"use strict";
-
 //1
-const c = 17;
-const f = 1.8 * c + 32;
-console.log(f);
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
 
 //2
-const days = 30;
-const hours = days * 24;
-const minute = hours * 60;
-console.log(hours);
-console.log(minute);
-
-//3
-let hp = 200;
-let energy = 200;
-hp -= 40;
-energy -= 70;
-console.log(hp);
-console.log(energy);
-
-//4
-let price = 4200;
-let sell = price / 100 * 90;
-console.log(sell);
-
-
-//5
-const v = 3.14;
-console.log(Math.floor(v));
-
-//6
-const t = '3.14pii12';
-console.log(parseFloat(t));
-
-//7
-const d = '3.1111ppppp';
-console.log(parseInt(d));
-
-//8
-const b = 16;
-console.log(Math.sqrt(b));
-
-//9
-const j = 10;
-const g = '12ppp';
-console.log(parseInt(g));
-console.log(j.toString());
+let email = 'pedcenkoulia2@gmail.com';
+console.log(email.includes('@'));
+console.log(email.length);
