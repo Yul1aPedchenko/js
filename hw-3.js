e89461ccaf1edf2fb3f4894ca5b083185eb90d9d
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-05-03 20:27:27 +0300

    hw-3 part 2/2

--- hw-3.js
+++ hw-3.js
@@ -8,3 +8,15 @@ console.log(typeof result);
 let email = "pedcenkoulia2@gmail.com";
 console.log(email.includes("@"));
 console.log(email.length);
+
+//3
+const one = 'My';
+const two = 'name';
+const three = 'is';
+const fullName = `${one} ${two} ${three} Victor`;
+console.log(fullName);
+
+//4
+const userName = 'Yul';
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
